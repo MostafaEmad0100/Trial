75e0708b39b43db7f8e70829049968a7a39af05d
moamen <engmoamen00@gmail.com>
2019-02-12 18:58:21 +0200

    commitNewController

---
+++ Trial/Controllers/MoamenController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Trial.Controllers
+{
+    public class MoamenController : Controller
+    {
+        // GET: Moamen
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
