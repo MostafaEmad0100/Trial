c385e4543d8f9bcb730ad9ed89503a7841c89ad1
moamen <engmoamen00@gmail.com>
2019-02-12 19:02:20 +0200

    MostafaEmadController

---
+++ Trial/Controllers/MostafaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Trial.Controllers
+{
+    public class MostafaController : Controller
+    {
+        // GET: Mostafa
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
